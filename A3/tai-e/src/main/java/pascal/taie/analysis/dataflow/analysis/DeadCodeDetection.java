f400f0a910cdb9a3f2d70ae3df1d7ca867fd1490
bossjoker1 <1397157763@qq.com>
2022-12-27 17:07:33 +0800

    finished T3 && halt the rest HWs

--- A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/DeadCodeDetection.java
+++ A3/tai-e/src/main/java/pascal/taie/analysis/dataflow/analysis/DeadCodeDetection.java
@@ -56,6 +56,87 @@ public class DeadCodeDetection extends MethodAnalysis {
         super(config);
     }
 
+//    @Override
+//    public Set<Stmt> analyze(IR ir) {
+//        // obtain CFG
+//        CFG<Stmt> cfg = ir.getResult(CFGBuilder.ID);
+//        // obtain result of constant propagation
+//        DataflowResult<Stmt, CPFact> constants =
+//                ir.getResult(ConstantPropagation.ID);
+//        // obtain result of live variable analysis
+//        DataflowResult<Stmt, SetFact<Var>> liveVars =
+//                ir.getResult(LiveVariableAnalysis.ID);
+//        // keep statements (dead code) sorted in the resulting set
+//        Set<Stmt> deadCode = new TreeSet<>(Comparator.comparing(Stmt::getIndex));
+//        // TODO - finish me
+//        // Your task is to recognize dead code in ir and add it to deadCode
+//
+//        Set<Stmt> liveCode = new TreeSet<>(Comparator.comparing(Stmt::getIndex));
+//
+//        // That means we just focus on one IR when doing dead code analyzing
+//        // 1. unreachable code
+//        // 1.1 control-flow: firstly add all nodes, and after we get the liveCode,
+//        // the rest of them are dead.
+//        deadCode.addAll(cfg.getNodes());
+//        // Because we should analyze all codes, we cannot use the iterator to iterate the cfg nodes
+//        // Here I also used queue.
+//        Queue<Stmt> q = new LinkedList<>();
+//        q.add(cfg.getEntry());
+//        while (!q.isEmpty()){
+//            Stmt stmt = q.poll();
+//            // dead variable
+//            // it must be assign Stmt and the LValue is Var.
+//            if (stmt instanceof AssignStmt<?, ?> as && as.getLValue() instanceof Var var){
+//                if(!liveVars.getResult(stmt).contains(var) && hasNoSideEffect(var)){
+//                    q.addAll(cfg.getSuccsOf(stmt));
+//                    continue;
+//                }
+//            }
+//
+//            if(!liveCode.add(stmt)){
+//                continue;
+//            }
+//            if(stmt instanceof If s){
+//                Value cond = ConstantPropagation.evaluate(s.getCondition(), constants.getInFact(stmt));
+//                if(cond.isConstant()){
+//                    for(Edge<Stmt> edge : cfg.getOutEdgesOf(stmt)) {
+//                        if((cond.getConstant() == 1 && edge.getKind() == Edge.Kind.IF_TRUE) ||
+//                                (cond.getConstant() == 0 && edge.getKind() == Edge.Kind.IF_FALSE)) {
+//                            q.add(edge.getTarget());
+//                        }
+//                    }
+//                }else{
+//                    q.addAll(cfg.getSuccsOf(stmt));
+//                }
+//            }else if(stmt instanceof SwitchStmt s){
+//                Value val = ConstantPropagation.evaluate(s.getVar(), constants.getInFact(stmt));
+//                if(val.isConstant()){
+//                    boolean hit = false;
+//                    for(Pair<Integer, Stmt> pair : s.getCaseTargets()){
+//                        if(pair.first() == val.getConstant()){
+//                            hit = true;
+//                            q.add(pair.second());
+//                        }
+//                    }
+//                    if(!hit){
+//                        q.add(s.getDefaultTarget());
+//                    }
+//                }else{
+//                    q.addAll(cfg.getSuccsOf(stmt));
+//                }
+//            }else{
+//                q.addAll(cfg.getSuccsOf(stmt));
+//            }
+//
+//        }
+//
+//        deadCode.addAll(cfg.getNodes());
+//        deadCode.removeAll(liveCode);
+//        deadCode.remove(cfg.getExit());
+//
+//        return deadCode;
+//    }
+
     @Override
     public Set<Stmt> analyze(IR ir) {
         // obtain CFG
@@ -68,31 +149,18 @@ public class DeadCodeDetection extends MethodAnalysis {
                 ir.getResult(LiveVariableAnalysis.ID);
         // keep statements (dead code) sorted in the resulting set
         Set<Stmt> deadCode = new TreeSet<>(Comparator.comparing(Stmt::getIndex));
-        // TODO - finish me
         // Your task is to recognize dead code in ir and add it to deadCode
-
         Set<Stmt> liveCode = new TreeSet<>(Comparator.comparing(Stmt::getIndex));
-
-        // That means we just focus on one IR when doing dead code analyzing
-        // 1. unreachable code
-        // 1.1 control-flow: firstly add all nodes, and after we get the liveCode,
-        // the rest of them are dead.
-        deadCode.addAll(cfg.getNodes());
-        // Because we should analyze all codes, we cannot use the iterator to iterate the cfg nodes
-        // Here I also used queue.
-        Queue<Stmt> q = new LinkedList<>();
-        q.add(cfg.getEntry());
-        while (!q.isEmpty()){
-            Stmt stmt = q.poll();
-            // dead variable
-            // it must be assign Stmt and the LValue is Var.
-            if (stmt instanceof AssignStmt<?, ?> as && as.getLValue() instanceof Var var){
-                if(!liveVars.getResult(stmt).contains(var) && hasNoSideEffect(var)){
-                    q.addAll(cfg.getSuccsOf(stmt));
+        Queue<Stmt> queue = new LinkedList<>();
+        queue.add(cfg.getEntry());
+        while(!queue.isEmpty()){
+            Stmt stmt = queue.poll();
+            if(stmt instanceof AssignStmt<?,?> s && s.getLValue() instanceof Var var) {
+                if(!liveVars.getResult(stmt).contains(var) && hasNoSideEffect(s.getRValue())) {
+                    queue.addAll(cfg.getSuccsOf(stmt));
                     continue;
                 }
             }
-
             if(!liveCode.add(stmt)){
                 continue;
             }
@@ -102,11 +170,11 @@ public class DeadCodeDetection extends MethodAnalysis {
                     for(Edge<Stmt> edge : cfg.getOutEdgesOf(stmt)) {
                         if((cond.getConstant() == 1 && edge.getKind() == Edge.Kind.IF_TRUE) ||
                                 (cond.getConstant() == 0 && edge.getKind() == Edge.Kind.IF_FALSE)) {
-                            q.add(edge.getTarget());
+                            queue.add(edge.getTarget());
                         }
                     }
                 }else{
-                    q.addAll(cfg.getSuccsOf(stmt));
+                    queue.addAll(cfg.getSuccsOf(stmt));
                 }
             }else if(stmt instanceof SwitchStmt s){
                 Value val = ConstantPropagation.evaluate(s.getVar(), constants.getInFact(stmt));
@@ -115,25 +183,22 @@ public class DeadCodeDetection extends MethodAnalysis {
                     for(Pair<Integer, Stmt> pair : s.getCaseTargets()){
                         if(pair.first() == val.getConstant()){
                             hit = true;
-                            q.add(pair.second());
+                            queue.add(pair.second());
                         }
                     }
                     if(!hit){
-                        q.add(s.getDefaultTarget());
+                        queue.add(s.getDefaultTarget());
                     }
                 }else{
-                    q.addAll(cfg.getSuccsOf(stmt));
+                    queue.addAll(cfg.getSuccsOf(stmt));
                 }
             }else{
-                q.addAll(cfg.getSuccsOf(stmt));
+                queue.addAll(cfg.getSuccsOf(stmt));
             }
-
         }
-
         deadCode.addAll(cfg.getNodes());
         deadCode.removeAll(liveCode);
         deadCode.remove(cfg.getExit());
-
         return deadCode;
     }
 
